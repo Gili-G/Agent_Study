ce0182f4cfd1f05bd501a2b65e28f7e7f970684d
Gili-G <ha309930@163.com>
2026-06-18 17:27:00 +0800

    6.18

--- chapter3/transformer/Encoder.py
+++ chapter3/transformer/Encoder.py
@@ -3,27 +3,68 @@ import torch
 import torch.nn as nn
 
 class Encoder(nn.Module):
-    def __init__(self, input_dim, dim, num_heads, N):
+    def __init__(self, seq_len, dim, num_heads, N, dropout=0.1):
         super(Encoder,self).__init__()
 
-        self.position_embedding = PositionEmdedding()
+        self.position_embedding = PositionEmbedding(dim, seq_len)
         self.multi_head_attention = MultiHeadAttention(num_heads, dim)
-        self.feed_forward = FeedForward()
+        self.feed_forward = FeedForward(dim)
         self.norm = nn.LayerNorm(dim)
         self.dropout = nn.Dropout(dropout)
-
-
+        self.N = N
 
     def forward(self, x):
         x = self.position_embedding(x)
         for _ in range(self.N):
-            x = self.norm(self.dropout(self.multi_head_attention(x)) + x)
+            x = self.norm(self.dropout(self.multi_head_attention(x,x,x)) + x)
             x = self.norm(self.dropout(self.feed_forward(x)) + x)
 
         return x
 
+class Decoder(nn.Module):
+    def __init__(self, seq_len, dim, num_heads, N, dropout=0.1):
+        super(Decoder, self).__init__()
+
+        self.position_embedding = PositionEmbedding(dim, seq_len)
+        self.multi_head_attention = MultiHeadAttention(num_heads, dim)
+        self.feed_forward = FeedForward(dim)
+        self.norm = nn.LayerNorm(dim)
+        self.dropout = nn.Dropout(dropout)
+        self.N = N
+
+    def forward(self, x, encoder_out):
+        x = self.position_embedding(x)
+        for _ in range(self.N):
+            x = self.norm(self.dropout(self.multi_head_attention(x,x,x,mask=True)) + x)
+            x = self.norm(self.dropout(self.multi_head_attention(x,encoder_out,encoder_out)) + x)
+            x = self.norm(self.dropout(self.feed_forward(x)) + x)
+
+        return x
+    
+class PositionEmbedding(nn.Module):
+    def __init__(self, dim, seq_len, dropout=0.1):
+        super(PositionEmbedding,self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pos = torch.arange(seq_len).unsqueeze(1)
+        term = torch.exp(torch.arange(0,dim,2)*(-torch.log(10000)/dim))
+
+        pe = torch.zeros(seq_len, dim)
+        pe[:, 0::2] = torch.sin(pos * term)
+        pe[:, 1::2] = torch.cos(pos * term)
+
+        # 将 pe 注册为 buffer，这样它就不会被视为模型参数，但会随模型移动（例如 to(device)）
+        self.register_buffer('pe', pe.unsqueeze(0))
+
+    def forward(self, x):
+        x = x + self.pe[:,:x.size(1)]
+        x = self.dropout(x)
+
+        return x
+
+
 class FeedForward(nn.Moudle):
-    def __int__(self, dim):
+    def __init__(self, dim, dropout=0.1):
         super(FeedForward,self).__int__()
 
         self.fc1 = nn.Linear(dim, dim*8)
@@ -36,7 +77,7 @@ class FeedForward(nn.Moudle):
 
 
 class MultiHeadAttention(nn.Module):
-    def __int__(self, num_heads, dim):
+    def __init__(self, num_heads, dim):
         super(MultiHeadAttention,self).__int__()
         assert dim//num_heads == 0
         self.dim_k = dim/num_heads
@@ -46,18 +87,21 @@ class MultiHeadAttention(nn.Module):
         self.W_V = nn.Linear(dim, dim)
         self.Out = nn.Linear(dim, dim)
 
-    def forward(self, x):
-        B, N, C = x.shape
+    def forward(self, q, k, v, mask=None):
+        B, N, C = q.shape
 
-        Q = self.W_Q(x)
-        K = self.W_K(x)
-        V = self.W_V(x)
+        Q = self.W_Q(q)
+        K = self.W_K(k)
+        V = self.W_V(v)
 
         Q_heads = Q.view(B, N, -1, C/self.num_heads).transpose(1, 2)
-        K_heads = Q.view(B, N, -1, C/self.num_heads).transpose(1, 2)
-        V_heads = Q.view(B, N, -1, C/self.num_heads).transpose(1, 2)
+        K_heads = K.view(B, N, -1, C/self.num_heads).transpose(1, 2)
+        V_heads = V.view(B, N, -1, C/self.num_heads).transpose(1, 2)
 
-        attn_out, attn_weights = self.scaled_dot_product_attention(Q_heads, K_heads, V_heads)
+        if mask is not None:
+            attn_out, attn_weights = self.scaled_dot_product_attention(Q_heads, K_heads, V_heads, mask=True)
+        else:
+            attn_out, attn_weights = self.scaled_dot_product_attention(Q_heads, K_heads, V_heads)
 
         attn_out = attn_out.transpose(1, 2).contiguous().view(B, N, -1)
         attn_out = self.Out(attn_out)
